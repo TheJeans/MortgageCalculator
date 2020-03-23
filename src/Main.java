4e021ae4a962af73f12ce99b12f89b5f2e6a0c40
Darian Glausier <darianglausier@gmail.com>
2020-03-23 10:01:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("What is the principal?: $");
+
+    }
+
+//    public static int Principal () {
+//
+//    }
+
+}
